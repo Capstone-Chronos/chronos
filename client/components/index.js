f13adc7ab64ee9b8f973b60add1d0ca392428b5f
Chung Lin <chunglin87@gmail.com>
2018-05-04 13:36:01 -0400

    route handling and alltimelines view

--- client/components/index.js
+++ client/components/index.js
@@ -3,3 +3,4 @@
 export { default as Navbar } from './Navbar.jsx';
 export { default as Footer } from './Footer.jsx';
 export { default as Main } from './Main.jsx';
+export { default as AllTimelines } from './AllTimelines'
