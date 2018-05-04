a9a04826ecb8deeecc7048a96ee13c477a6fab6b
Chung Lin <chunglin87@gmail.com>
2018-05-04 13:06:40 -0400

    route handling and all timelines view

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const express = require('express')
+const path = require('path')
+
 
 const app = express()
 
@@ -7,6 +9,12 @@ app.get('/hi', (req, res, next) => {
     console.log('HELLOOOOOOOOO')
     res.send('HIIIII')
 })
+
+// sends index.html
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
