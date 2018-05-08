3542f0afb33f009da1e748db9dca69a5bc0c91c1
Mario Villacreses <MarioVillacreses@outlook.com>
2018-05-08 12:46:30 -0400

    Uninstall express

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,9 @@
 const functions = require('firebase-functions');
-const express = require('express')
 const path = require('path')
 
-
-const app = express()
-
-app.get('/hi', (req, res, next) => {
-    console.log('HELLOOOOOOOOO')
-    res.send('HIIIII')
-})
-
-// sends index.html
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  })
-
+functions.https.onRequest((req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.app = functions.https.onRequest(app);
