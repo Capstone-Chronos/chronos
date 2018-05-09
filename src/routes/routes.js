1f991fcffe7ea9498142b70ccbca7310295d089c
BxBomber999 <coyle.peters@gmail.com>
2018-05-09 18:24:51 -0400

    resolved dependencies

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,17 @@ import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ChartContainer from '../components/ChartContainer';
-import { Navbar, Footer, Main, AllTimelines, EditTimeline, SignUp, Login, BarChart, SankeyWrapper } from '../components';
+import {
+  Navbar,
+  Footer,
+  Main,
+  AllTimelines,
+  EditTimeline,
+  SignUp,
+  Login,
+  BarChart,
+  SankeyWrapper
+} from '../components';
 
 /**
  * COMPONENT
@@ -22,17 +32,13 @@ class Routes extends Component {
           {/* <Route exact path="/create" component={EditTimeline} /> */}
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={Login} />
-<<<<<<< HEAD
+          <Route path="/edit" component={ChartContainer} />
+          <Route exact path="/sankey" component={SankeyWrapper} />
           <Route
             exact
             path="/barchart"
             render={() => <BarChart data={[5, 10, 1, 3]} size={[500, 500]} />}
           />
-          <Route path="/edit" component={ChartContainer} />
-=======
-          <Route exact path="/sankey" component={SankeyWrapper} />
-          <Route exact path="/barchart" render={() => <BarChart data={[5,10,1,3]} size={[500,500]}/>} />
->>>>>>> master
         </Switch>
       </div>
     );
