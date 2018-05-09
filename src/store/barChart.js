9673317889224e2542fe560ea6358fc455918721
BxBomber999 <coyle.peters@gmail.com>
2018-05-09 18:04:27 -0400

    add item started

--- src/store/barChart.js
+++ src/store/barChart.js
@@ -6,6 +6,7 @@ const initialState = {
 
 const LOAD_DEFAULT_DATA = 'LOAD_DEFAULT_DATA';
 const CHANGE_TEMP_VAL = 'CHANGE_TEMP_VAL';
+const ADD_DATA_POINT = 'ADD_DATA_POINT';
 
 const defaultData = [3, 7, 5, 10];
 
@@ -13,13 +14,22 @@ export const loadDefaultData = () => ({
   type: LOAD_DEFAULT_DATA,
   data: defaultData
 });
-const changeTempVal = tempVal => ({ type: CHANGE_TEMP_VAL, tempVal });
+const addDataPoint = point => {
+  return {
+    type: ADD_DATA_POINT,
+    point
+  };
+};
+
+// const changeTempVal = tempVal => ({ type: CHANGE_TEMP_VAL, tempVal });
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_DEFAULT_DATA:
       console.log(action.data, 'DEFAULT DATA PUT IN STORE');
       return { ...state, data: action.data };
+    case ADD_DATA_POINT:
+      return { ...state, data: state.data.concat([action.point]) };
     default:
       return state;
   }
