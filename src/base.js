a58c52598ac4c1df4200e3f008bbe3324b81b871
Chung Lin <chunglin87@gmail.com>
2018-05-10 17:54:09 -0400

    it saves to firebase!

--- src/base.js
+++ src/base.js
@@ -1,11 +1,32 @@
-import Rebase from 're-base';
+// import Rebase from 're-base';
 import firebase from 'firebase';
-import configObj from './secrets';
 
-const config = configObj;
+// const config = {
+//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+// };
+
+const config = {
+  apiKey: "AIzaSyC9SQR3zvO9dMq4tMHAW1GtGnDOGPdTyqU",
+  authDomain: "chronos-d08ff.firebaseapp.com",
+  databaseURL: "https://chronos-d08ff.firebaseio.com",
+  projectId: "chronos-d08ff",
+  storageBucket: "chronos-d08ff.appspot.com",
+  messagingSenderId: "915480064490"
+};
 
 const app = firebase.initializeApp(config);
-const base = Rebase.createClass(app.database());
+// const base = Rebase.createClass(app.database());
+
+const databaseRef = firebase.database().ref();
+
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-export { app, base, googleProvider };
+const sankeyRef = databaseRef.child('sankey');
+
+
+export { app, sankeyRef, googleProvider };
