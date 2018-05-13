ecd9c54187d5dcf0c7ca2b97fc179c05fbf624bb
BxBomber999 <coyle.peters@gmail.com>
2018-05-13 01:45:32 -0400

    merge master

--- src/routes/Toolbar.js
+++ src/routes/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
-import { BarChartTools, SankeyTools } from '../components';
+import { SankeyTools } from '../components';
+import { BarChartTools } from '../components/toolbars';
 
 const Toolbar = () => (
   <Switch>
