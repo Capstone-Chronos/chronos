04f125ac7cf77bfac131916814e1645b461f75be
Chung Lin <chunglin87@gmail.com>
2018-05-14 10:39:59 -0400

    can link to single views

--- src/components/layout/SingleSelection.js
+++ src/components/layout/SingleSelection.js
@@ -6,8 +6,10 @@ const SingleSelection = props => (
   <Card className="single-card" as={Link} to={props.url}>
     <Image src="https://www.fillmurray.com/300/200" centered size="medium" />
     <Card.Content>
+    {console.log('single selection', props)}
+
       <Card.Header>{props.name}</Card.Header>
-      <Card.Meta>Meta information goes here</Card.Meta>
+      <Card.Meta>Type: {props.type}</Card.Meta>
       <Card.Description>The description goes here.</Card.Description>
     </Card.Content>
   </Card>
