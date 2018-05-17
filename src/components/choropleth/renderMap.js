1dbdb37502499d838a34eea570dfa64db4d120ca
Mario Villacreses <MarioVillacreses@outlook.com>
2018-05-17 10:44:25 -0400

    Organize renderMap so that auto-coloring becomes an option

--- src/components/choropleth/renderMap.js
+++ src/components/choropleth/renderMap.js
@@ -2,13 +2,18 @@ import { select, event } from 'd3-selection';
 import { json as readJSON } from 'd3';
 import { mesh, feature } from 'topojson';
 import { geoPath } from 'd3-geo';
+import { colorScale as scale } from '../toolbars/tools';
 
 const hoverWizard = data => {
   let [x, y] = [event.layerX, event.layerY];
 };
 
-export default function renderMap (toggleModal, stateColors) {
+export default function renderMap (toggleModal, stateColors, rank, stateData) {
   const geoFileUrl = 'https://d3js.org/us-10m.v1.json';
+
+  const findRank = datum =>
+    rank.length - rank.filter(r => (r >= datum)).length;
+  const stateColor = stateId => scale[findRank(stateData[stateId])];
 
   return readJSON(geoFileUrl, (err, us) => {
     if (err) throw err;
@@ -18,7 +23,6 @@ export default function renderMap (toggleModal, stateColors) {
     const geoData = feature(us, states).features;
     const path = geoPath();
     const lines = path(mesh(us, states, (a, b) => (a !== b)));
-    let tooltip = document.getElementById('tooltip-container');
 
     select(node)
       .append('g')
@@ -31,9 +35,11 @@ export default function renderMap (toggleModal, stateColors) {
       .attr('id', data => 'state' + Number(data.id))
       .on('mousemove', data => hoverWizard(data))
       .on('click', data => toggleModal(data.id))
-      .style('fill', function (data) {
-        return stateColors[data.id] || '#939393';
-      });
+      .style('fill', data =>
+        stateColors[data.id] ||
+        rank && stateData && stateColor[data.id] ||
+        '#939393'
+      );
 
     select(node)
       .append('path')
